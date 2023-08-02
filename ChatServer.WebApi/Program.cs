023c28779428dd01f32f427ae6fbba37cb76adef
= <=>
2023-08-02 20:17:46 +0200

    changes

--- ChatServer.WebApi/Program.cs
+++ ChatServer.WebApi/Program.cs
@@ -1,9 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddSignalR();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors();
+builder.Services.AddSignalR();
+builder.Services.AddSingleton<ChatHub>();
 
 var app = builder.Build();
 
@@ -14,31 +19,82 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors(x => x
+    .AllowAnyMethod()
+    .AllowAnyHeader()
+    .SetIsOriginAllowed(origin => true)
+    .AllowCredentials());
+
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+app.MapHub<ChatHub>("/chatHub");
 
-app.MapGet("/weatherforecast", () =>
+app.MapPost("Rooms/CreateOffer", async ([FromBody] CreateOfferRequest request) => 
 {
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
+    var room = new Room(request.Offer);
+    Db.Rooms.Add(room);
+    await Task.CompletedTask;
+    return new CreateOfferResponse(room.Id);
+});
+
+app.MapPost("Rooms/{id}/AddCandidate", async ([FromRoute]Guid id, [FromBody] AddCandidateRequest request, ChatHub hub) =>
+{
+    var room = Db.Rooms.First(x => x.Id == id);
+    room.Candidates.Add(new Candidate(request.Candidate));
+    await hub.CandidateAddedToRoom(room.Id, request.Candidate);
+});
+
+app.MapPost("Rooms/{id}/AddAnswer", async ([FromRoute] Guid id, [FromBody] AddAnswerRequest request, ChatHub hub) =>
+{
+    var room = Db.Rooms.First(x => x.Id == id);
+    room.Answer = request.Answer;
+    await hub.AnswerAddedToRoom(room.Id, request.Answer);
+});
+
+app.MapPost("Rooms/{id}/GetOffer", async ([FromRoute] Guid id) => 
+{
+    var room = Db.Rooms.First(x => x.Id == id);
+    await Task.CompletedTask;
+    return new GetOfferResponse(room.Offer);
+});
+
 
 app.Run();
 
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+class ChatHub : Hub
 {
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    public async Task AnswerAddedToRoom(Guid roomId, SdpData answer)
+    {
+        await Clients.All.SendAsync("AnswerAddedToRoom", roomId, answer);
+    }
+
+    public async Task CandidateAddedToRoom(Guid roomId, CandidateItem candidate)
+    {
+        await Clients.All.SendAsync("CandidateAddedToRoom", roomId, candidate);
+    }
 }
+
+record CreateOfferRequest(SdpData Offer);
+record CreateOfferResponse(Guid RoomId);
+record AddCandidateRequest(CandidateItem Candidate);
+record AddAnswerRequest(SdpData Answer);
+record GetOfferResponse(SdpData offer);
+
+static class Db
+{
+    public static List<Room> Rooms { get; } = new();
+}
+
+
+record Entity { public Guid Id { get; } = Guid.NewGuid(); }
+
+record Candidate(CandidateItem Data) : Entity;
+
+record Room(SdpData Offer) : Entity
+{ 
+    public List<Candidate> Candidates { get; } = new();
+    public SdpData? Answer { get; set; }
+}
+
+record SdpData(string sdp, string type);
+record CandidateItem(string candidate, int sdpMLineIndex, string sdpMid, string usernameFragment);
